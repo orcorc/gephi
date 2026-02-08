ff7fef7f5b80da436a92f9c8a9c1eabf3b327fe5
Mathieu Bastian <mathieu.bastian@gmail.com>
2026-02-08 11:15:15 +0100

    Javadoc

--- modules/PreviewPlugin/src/main/java/org/gephi/preview/plugin/renderers/SizeUtils.java
+++ modules/PreviewPlugin/src/main/java/org/gephi/preview/plugin/renderers/SizeUtils.java
@@ -7,6 +7,13 @@ import org.gephi.preview.plugin.items.NodeItem;
 
 public class SizeUtils {
 
+    /**
+     * Get the node size to render, taking into account the node size and the node scale factor.
+     *
+     * @param item the node item
+     * @param properties the preview properties
+     * @return the node size to render
+     */
     public static float getNodeSize(Item item, PreviewProperties properties) {
         float scale = properties.getFloatValue(PreviewProperty.NODE_SCALE_FACTOR);
         Float size = item.getData(NodeItem.SIZE);
